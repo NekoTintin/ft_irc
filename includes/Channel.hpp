8fea898ddd99fcd9ef3384052c2ba7bfd2bccc48
qupollet <qupollet@f1r4s18.paris.42.school>
2026-06-02 21:12:54 +0200

    Started Channel class

---
+++ includes/Channel.hpp
@@ -0,0 +1,16 @@
+#ifndef CHANNEL_HPP
+# define CHANNEL_HPP
+
+#include <iostream>
+#include <string>
+
+class Channel {
+	public:
+		Channel();
+		~Channel();
+	private:
+		std::string _name;
+		std::string _topic;
+}
+
+# endif
