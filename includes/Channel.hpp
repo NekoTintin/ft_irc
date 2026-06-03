c49da4fa3519e2a87741ebc60711133c730fca27
qupollet <qupollet@f1r5s17.paris.42.school>
2026-06-03 14:43:47 +0200

    Finished Channel hpp

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -3,14 +3,25 @@
 
 #include <iostream>
 #include <string>
+#include <map>
+#include <vector>
 
 class Channel {
 	public:
 		Channel();
 		~Channel();
 	private:
+		// Basics
 		std::string _name;
 		std::string _topic;
+		// Users
+		std::map<int, const Client*> _users;
+		std::vector<int> _operators;
+		// Modes
+		bool			_isInviteOnly;
+		bool			_isProtectedTopic;
+		std::string		channelKey;
+		std::size_t		_userLimit;
 };
 
 # endif
