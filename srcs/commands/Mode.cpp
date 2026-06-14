86a5e5a492df9a8fea905be6d53f61b961e849c3
mlelu <mlelu@f1r3s15.paris.42.school>
2026-06-14 23:35:10 +0200

    +

--- srcs/commands/Mode.cpp
+++ srcs/commands/Mode.cpp
@@ -1,16 +1,3 @@
-/*  5 modes disponibles
-i : set/remove invite only
-t : seul operateur peut changer le topic
-k : set/remove mdp channel
-o : give/take channel operator privilege
-l : set/remove limite utilisateurs
-
-	MODE #42 +k motdepasse
-	MODE #42 +o marie
-	MODE #42 +l 10
-	MODE #42 -i
-*/
-
 #include "commands/Mode.hpp"
 #include "Server.hpp"
 #include "Client.hpp"
