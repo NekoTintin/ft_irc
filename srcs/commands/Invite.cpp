202c18f78dc59f00088aecf098465f6781a30ece
mlelu <mlelu@f1r3s15.paris.42.school>
2026-06-14 23:37:11 +0200

    +

--- srcs/commands/Invite.cpp
+++ srcs/commands/Invite.cpp
@@ -11,7 +11,6 @@
 
 bool handleInvite(std::vector<std::string> &Token, Server &server, Client &client, bool _hasTrailing) {
 	std::cout << "HANDLE INVITE" << std::endl;
-	(void)Token;
 	(void)_hasTrailing;
 	
 	// Check if client is registered
@@ -21,6 +20,52 @@ bool handleInvite(std::vector<std::string> &Token, Server &server, Client &clien
 		return (false);
 	}
 
-	
+	// Verify arguments
+	if (Token.size() < 3) {
+		server.sendToClient(client.getFd(), ERR_NEEDMOREPARAMS("INVITE"));
+		std::cerr << "INVITE HANDLER - Not enough parameters" << std::endl;
+		return (false);
+	}
+
+	// Is channel valid ?
+	Channel *channel = server.findChannel(Token[2]);
+	if (!channel) {
+		server.sendToClient(client.getFd(), ERR_NOSUCHCHANNEL(Token[2]));
+		std::cerr << "INVITE HANDLER - Channel does not exist" << std::endl;
+		return (false);
+	}
+
+	// Is client on channel ?
+	if (!channel->isUserOnChannel(&client)) {
+		server.sendToClient(client.getFd(), ERR_NOTONCHANNEL(Token[2]));
+		std::cerr << "INVITE HANDLER - Client is not on channel" << std::endl;
+		return (false);
+	}
+
+	// Is client an operator on channel ?
+	if (!channel->isUserOperator(&client)) {
+		server.sendToClient(client.getFd(), ERR_CHANOPRIVSNEEDED(Token[2]));
+		std::cerr << "INVITE HANDLER - Client is not an operator on channel" << std::endl;
+		return (false);
+	}
+
+	// Is target client valid ?
+	Client *targetClient = server.findClient(Token[1]);
+	if (!targetClient) {
+		server.sendToClient(client.getFd(), ERR_NOSUCHNICK(Token[1]));
+		std::cerr << "INVITE HANDLER - Target client does not exist" << std::endl;
+		return (false);
+	}
+
+	// Check if target client is already on the channel
+	if (channel->isUserOnChannel(targetClient)) {
+		server.sendToClient(client.getFd(), ERR_USERONCHANNEL(targetClient->getNickname(), targetClient->getNickname(), channel->getName()));
+		std::cerr << "INVITE HANDLER - Target client is already on channel" << std::endl;
+		return (false);
+	}
+
+	// Add to invited list
+	channel->addToInvitedList(targetClient->getNickname());
+
 	return (true);
 }
